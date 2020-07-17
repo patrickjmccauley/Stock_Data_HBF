47c68d9a4574564a69d79a1f9448d910ead84806
Patrick McCauley <pmccau@seas.upenn.edu>
2020-07-17 14:57:27 -0400

    Added in name parsing from Yahoo finance, added in combined index of all companies, switched some companies around that weren't pulling data properly and added some misc cleaning. Also switched run interval to 20 min, since it appears Alpha Vantage changed their API and now only shows the prior day's quote (not real-time), so the data will always be a day old. No need to refresh every 15 minutes (or really at all), but hoping this will capture the current day's data after trading ends (ex. script runs at 4:00 or 4:20pm). Finally uncommented out the info needed for heroku

--- BuildIndex.py
+++ BuildIndex.py
@@ -2,7 +2,7 @@ import requests, json, time, os, sys, ftplib
 import alpha_vantage
 from datetime import date, datetime, timezone, timedelta
 import datetime as dt
-
+import traceback
 
 DEBUG_MODE = False
 
@@ -12,8 +12,8 @@ def query_alpha_vantage(av_symbol, API_KEY):
 
     Official documentation here: https://www.alphavantage.co/documentation/
 	"""
-	log("Querying for {}".format(av_symbol))
 	while True:
+		log("Querying for '{}'".format(av_symbol))
 		url = "https://www.alphavantage.co/query"
 		data = {
 			"function": "GLOBAL_QUOTE",
@@ -23,33 +23,35 @@ def query_alpha_vantage(av_symbol, API_KEY):
 		r = requests.get(url, params=data)
 		log("Response: {}: {}".format(r.status_code, r.reason))
 		if r.status_code == 200:
-			if 'Global Quote' in json.loads(r.content).keys():
-				print('{}\t{}: {}'.format(av_symbol, r.status_code, r.reason))
-				return r
+			contents = json.loads(r.content)
+			log("Logging the response...")
+			if 'Global Quote' in contents.keys():
+				log('{}\t{}: {}'.format(av_symbol, r.status_code, r.reason))
+				return contents
+			elif 'Note' in contents.keys():
+				print("{}\t{}: Hit rate limit. Sleeping for 60 seconds".format(av_symbol, r.status_code, r.reason))
+				log("Waiting 60 seconds due to rate limit")
+				time.sleep(60)
+				continue
 			else:
-				content = json.loads(r.content)
-				if 'Error Message' in content.keys():
-					print('{}\t{}: {}\tError on request: {}'.format(av_symbol, r.status_code, r.reason, content['Error Message']))
-					return None
-				else:
-					print("{}\t{}: Hit rate limit. Sleeping for 60 seconds".format(av_symbol, r.status_code, r.reason))
-					log("Waiting 60 seconds due to rate limit")
-					time.sleep(60)
-					continue
+				log("Appear to have hit some erroneous response in a 200 code")
+				print('{}\t{}: {}\tError on request: {}'.format(av_symbol, r.status_code, r.reason,
+																contents['Error Message']))
+				return None
 		else:
-			log('Error parsing {}\t{}: {}'.format(av_symbol, r.status_code, r.reason))
+			log('Error querying AV {}\t{}: {}'.format(av_symbol, r.status_code, r.reason))
 			return None
 
 
-def parse_av_response(r):
+def parse_av_response(contents):
 	""" This will parse through the request passed back from the Global Quote api endpoint.
 	It will return a dictionary object
 	"""
-	if r is None:
+	if contents is None:
 		return None
 	try:
-		trading_data = json.loads(r.content)['Global Quote']
-		
+		trading_data = contents['Global Quote']
+		log("Trading data = {}".format(trading_data))
 		output = {
 			"price": float(trading_data['05. price']),
 			"previous_close": float(trading_data['08. previous close']),
@@ -60,50 +62,63 @@ def parse_av_response(r):
 			"open": float(trading_data['02. open']),
 			"refresh_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 		}
-		output["change_from_open"] = output["price"] - output["open"]
-		output["change_from_open_percent"] = output["change_from_open"] / output["open"]
+		output["change_from_open"] = output["price"] - output["previous_close"]
+		output["change_from_open_percent"] = output["change_from_open"] / output["previous_close"]
 		return output
 	except Exception as e:
-		log("Error trying to parse alpha vantage response: {}".format(r), e)
+		print(e)
+		log("Error trying to parse alpha vantage response: {}".format(contents), e)
 		return None
 
 
 def query_yahoo_finance(symbol):
-	""" This function will scrape the yahoo finance page for the shares outstanding, which
+    """ This function will scrape the yahoo finance page for the shares outstanding, which
 	will be used to calculate market cap
 	"""
-	url = 'https://finance.yahoo.com/quote/{}/key-statistics?p={}'.format(symbol, symbol)
-	r = requests.get(url)
-	content = str(r.content)
-	
-	# Chop up the response to find the part we're interested in
-	i = content.find('Shares Outstanding')
-	content = content[i:]
-	i = content.find('</td>') + 5
-	content = content[i:]
-	i = content.find('</td>') - 10
-	content = content[i:content.find('</td')]
-	i = content.find('>')
+    url = 'https://finance.yahoo.com/quote/{}/key-statistics?p={}'.format(symbol, symbol)
+    r = requests.get(url)
+    content = str(r.content)
 
-	shares_outstanding = content[i + 1:]
+    # Pull the name
+    name_content = content
+    i = name_content.find('quote-header-info')
+    name_content = name_content[i:]
+    i = name_content.find('<h1')
+    name_content = name_content[i:]
+    i = name_content.find('>')
+    name_content = name_content[i + 1:]
+    i = name_content.find('<')
+    name = name_content[:i].strip()
 
-	shares, denomination = shares_outstanding[:len(shares_outstanding) - 1], shares_outstanding[-1]
-	if denomination == 'M':
-		shares_outstanding = float(shares) * 1000000
-	elif denomination == 'B':
-		shares_outstanding = float(shares) * 1000000000
-	else:
-		shares_outstanding = -1
+    # Pull shares outstanding
+    i = content.find('Shares Outstanding')
+    content = content[i:]
+    i = content.find('</td>') + 5
+    content = content[i:]
+    i = content.find('</td>') - 10
+    content = content[i:content.find('</td')]
+    i = content.find('>')
 
-	return shares_outstanding
+    shares_outstanding = content[i + 1:]
+
+    shares, denomination = shares_outstanding[:len(shares_outstanding) - 1], shares_outstanding[-1]
+    if denomination == 'M':
+        shares_outstanding = float(shares) * 1000000
+    elif denomination == 'B':
+        shares_outstanding = float(shares) * 1000000000
+    else:
+        shares_outstanding = -1
+
+    return shares_outstanding, name
 
 
 def populate_market_cap(symbol, contents):
 	""" Simple helper to calculate the market cap and shares outstanding
 	"""
 	try:
-		contents["shares_outstanding"] = query_yahoo_finance(symbol)
+		contents["shares_outstanding"], name = query_yahoo_finance(symbol)
 		contents["market_cap"] = contents["price"] * contents["shares_outstanding"]
+		contents["name"] = name
 	except Exception as e:
 		log("Error trying to populate market cap for {}".format(symbol), e)
 		contents["shares_outstanding"] = 0
@@ -125,6 +140,7 @@ def generate_html(tickers_json, css_file):
 	# Add the individual stocks
 	for symbol in tickers_json:
 		symbol_data = tickers_json[symbol]
+		name = symbol_data['name']
 		price, change_from_open_percent = symbol_data["price"], symbol_data["change_from_open_percent"]
 
 		if change_from_open_percent < 0:
@@ -145,7 +161,7 @@ def generate_html(tickers_json, css_file):
 			<span class="tickerValue"> ${:.2f}</span>&nbsp;&nbsp;\n\t\t\
 			<span class="tickerPercent {}">{:.3%}</span>&nbsp;\n\t\t\
 			<span class="tickerDirection {}">{}</span>\n\t\t
-			""".format(symbol, price, change_color, change_from_open_percent, change_color, direction)
+			""".format(name, price, change_color, change_from_open_percent, change_color, direction)
 		output += "<div class=\"ticker__item\">{}</div>".format(this_ticker)
 
 	output += """	</div>
@@ -153,6 +169,41 @@ def generate_html(tickers_json, css_file):
 		"""
 	log("HTML created")
 	return output
+
+def update_index_data(current_data, idx_symbol):
+	""" This function is used to build a condensed 'index' value of all the market fluctuations across companies.
+	The divisor is arbitrary (see price)
+	"""
+	log("UPDATING THE INDEX DATA... SYMBOL={}".format(idx_symbol))
+	current_mkt_cap = 0
+	open_mkt_cap = 0
+	for symbol in current_data:
+		if symbol != idx_symbol:
+			cur_mkt_cap = current_data[symbol]['market_cap']
+			change_pct = current_data[symbol]['change_from_open_percent']
+			op_mkt_cap = cur_mkt_cap - (change_pct * cur_mkt_cap)
+
+			open_mkt_cap += op_mkt_cap
+			current_mkt_cap += cur_mkt_cap
+
+	current_data[idx_symbol] = {
+		"price": current_mkt_cap / 30000000,
+		"previous_close": -1,
+		"high": -1,
+		"trading_day": "calculated",
+		"low": -1,
+		"volume": -1,
+		"open": open_mkt_cap,
+		"refresh_time": "calculated",
+		"change_from_open": current_mkt_cap - open_mkt_cap,
+		"change_from_open_percent": (current_mkt_cap - open_mkt_cap) / open_mkt_cap,
+		"shares_outstanding": -1,
+		"market_cap": current_mkt_cap,
+		"name": "{} - Hepatitis B Research Index".format(idx_symbol)
+	}
+
+	log("HBRI BELOW")
+	log(json.dumps(current_data[idx_symbol], indent=2))
 
 
 def validate_time(now):
@@ -184,12 +235,13 @@ def validate_time(now):
 def log(msg, err=None):
 	""" Log locally to a file with message and optional error inclusion 
 	"""
- 	
 	now = datetime.now
 	tz = timezone(-timedelta(hours=4))
 	time = now(tz=tz).strftime("%Y-%m-%d %H:%M:%S %Z")
 
 	to_write = '{} > {}\n'.format(time, msg)
+	if err is not None:
+		to_write += '{}\n'.format(traceback.print_exc())
 	if DEBUG_MODE:
 		print(to_write)
 	f = open('./log.txt', 'a+')
@@ -211,6 +263,7 @@ def main():
 	css_file = "{}.css".format(filename_prefix)
 	html_file = "{}.html".format(filename_prefix)
 	input_file = "{}.csv".format(filename_prefix)
+	index_symbol = 'HBRI'
 
 	# Infinite loop for calculations. This will rest when rate limit is hit,
 	# or if it's not a valid trading time (markets are closed for weekend,
@@ -241,12 +294,22 @@ def main():
 		API_KEY = os.environ['AV_API_KEY']
 		for i in range(len(contents)):
 			symbol = contents[i]
+			symbol = symbol.strip()
+			symbol = symbol.replace('\n', '')
+			if symbol == index_symbol:
+				current_data[symbol] = {}
+				continue
 			data = parse_av_response(query_alpha_vantage(symbol, API_KEY))
 			if data is None:
 				continue
 			else:
 				current_data[symbol] = data
 				populate_market_cap(symbol, current_data[symbol])
+
+		# Update the overall index value
+		update_index_data(current_data, index_symbol)
+
+		# Save this data
 		if len(current_data) != 0:
 			f.write(json.dumps(current_data, indent=2))
 		f.close()
@@ -258,8 +321,8 @@ def main():
 
 		upload()
 
-		# Sleep for 15 minutes, then repeat
-		time.sleep(15 * 60)
+		# Sleep for 20 minutes, then repeat
+		time.sleep(20 * 60)
 
 if __name__ == "__main__":
 	main()
